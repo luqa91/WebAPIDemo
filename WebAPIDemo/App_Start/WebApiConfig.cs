b02000f666cc02e693ff39d6106d431e78b84169
Łukasz <luqa91@hotmail.com>
2017-05-17 21:48:46 +0200

    Implementing Post method

--- WebAPIDemo/App_Start/WebApiConfig.cs
+++ WebAPIDemo/App_Start/WebApiConfig.cs
@@ -5,9 +5,27 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 
 namespace WebAPIDemo
 {
+    public class CustomJsonFormatter : JsonMediaTypeFormatter
+    {
+
+        //public CustomJsonFormatter()
+        //{
+        //this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        //}
+
+        //public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        //{
+        //    base.SetDefaultContentHeaders(type, headers, mediaType);
+        //    headers.ContentType = new MediaTypeHeaderValue("application/json");
+        //}
+    }
+
+    
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -25,6 +43,18 @@ namespace WebAPIDemo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        //      config.Formatters.Add(new CustomJsonFormatter());
+
+        //      config.Formatters.Remove(config.Formatters.XmlFormatter);
+        //      config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+        //      config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+        //      config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+
+
+
+
+
         }
     }
 }
---
+++ WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,67 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (WebApiDemoEntities db = new WebApiDemoEntities())
+            {
+                return db.Employees.ToList();
+            }
+
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            using (WebApiDemoEntities db = new WebApiDemoEntities())
+            {
+                var entity = db.Employees.FirstOrDefault(e => e.ID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id: " + id.ToString() + " not found");
+                }
+
+            }
+
+        }
+
+        public HttpResponseMessage Post([FromBody]Employee employee)
+        {
+
+            try
+            {
+            using (WebApiDemoEntities db = new WebApiDemoEntities())
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+
+
+                var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                return message;
+            }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+
+    }
+}
